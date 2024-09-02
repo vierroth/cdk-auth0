73b386b9f3cb06d43548880af8056416fc76d47e
Luis Vierroth <derluis@me.com>
2024-09-02 19:08:31 +0300

    Added import.meta.dirname as alternative to __dirname

--- src/trigger/provider.ts
+++ src/trigger/provider.ts
@@ -10,7 +10,17 @@ export class Provider extends AwsProvider {
   constructor(scope: Construct, id: string) {
     super(scope, id, {
       onEventHandler: new LambdaBase(scope, `${id}OnEventHandler`, {
-        entry: join(__dirname, "./../../src/trigger/handler.ts"),
+        entry: join(
+          (() => {
+            try {
+              return __dirname;
+            } catch (_) {
+              // @ts-ignore
+              return import.meta.dirname;
+            }
+          })(),
+          "./../../src/trigger/handler.ts",
+        ),
       }),
       role: new LambdaRole(scope, `${id}Role`),
     });
